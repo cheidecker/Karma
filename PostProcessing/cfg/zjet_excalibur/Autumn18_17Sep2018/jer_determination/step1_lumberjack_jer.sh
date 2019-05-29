cf40fa10518638f595ee3192587ce2c8f3784cff
Christoph Heidecker <christoph.heidecker@cern.ch>
2019-05-29 17:13:32 +0200

    [Postprocessing][zjet_excalibur][JER] Differ between 2018 runs ABCD, runs ABC and run D in Lumberjack starting script

--- PostProcessing/cfg/zjet_excalibur/Autumn18_17Sep2018/jer_determination/step1_lumberjack_jer.sh
+++ PostProcessing/cfg/zjet_excalibur/Autumn18_17Sep2018/jer_determination/step1_lumberjack_jer.sh
@@ -8,19 +8,27 @@ source `which with_root_df`
 # -- step1: use Lumberjack to create "pre-combination" files from Excalibur TTrees
 # Note: if these already exist, they are skipped, unless `--overwrite` flag is passed
 
-SAMPLE_DIR="/ceph/dsavoiu/JEC/Autumn18/17Sep2018_V5_2019-02-26"
+# SAMPLE_DIR="/ceph/dsavoiu/JEC/Autumn18/17Sep2018_V5_2019-02-26"
+# DATA_VERSION="17Sep2018"
+# JEC_VERSION="Autumn18_JECV5"
+SAMPLE_DIR="/portal/ekpbms3/home/cheidecker/excalibur_work/excalibur_rootfiles"
+DATA_VERSION="17Sep2018"
+JEC_VERSION="Autumn18_JECV8"
 
-for _ch in "mm" "ee"; do
-    INFILE_DATA="${SAMPLE_DIR}/data18_${_ch}_ABCD_17Sep2018.root"
+# for _ch in "mm" "ee"; do
+for _ch in "mm"; do
+    INFILE_DATA_ABCD="${SAMPLE_DIR}/data18_${_ch}_ABCD_${DATA_VERSION}.root"
+    INFILE_DATA_ABC="${SAMPLE_DIR}/data18_${_ch}_ABC_${DATA_VERSION}.root"
+    INFILE_DATA_D="${SAMPLE_DIR}/data18_${_ch}_D_${DATA_VERSION}.root"
     INFILE_MC="${SAMPLE_DIR}/mc18_${_ch}_DYNJ_Madgraph.root"
 
     # -- MC
     for _corr_level in "L1L2L3"; do
-        OUTPUT_FILE_SUFFIX="Z${_ch}_17Sep2018_Autumn18_JECV5_${_corr_level}"
+        OUTPUT_FILE_SUFFIX="Z${_ch}_${DATA_VERSION}_${JEC_VERSION}_${_corr_level}"
 
         lumberjack.py -a zjet_excalibur -i "$INFILE_MC" \
             --tree "basiccuts_${_corr_level}/ntuple" \
-            --type mc \
+            --input-type mc \
             --selections "alpha" \
             -j10 \
             --log --progress \
@@ -29,14 +37,46 @@ for _ch in "mm" "ee"; do
             --output-file-suffix "$OUTPUT_FILE_SUFFIX"
     done
 
+    # -- DATA (runs A, B, C, D with corr level L1L2Res)
+
+    for _corr_level in "L1L2Res"; do
+        OUTPUT_FILE_SUFFIX="ABCD_Z${_ch}_${DATA_VERSION}_${JEC_VERSION}_${_corr_level}"
+
+        lumberjack.py -a zjet_excalibur -i "$INFILE_DATA_ABCD" \
+            --tree "basiccuts_${_corr_level}/ntuple" \
+            --input-type data \
+            --selections "alpha" \
+            -j10 \
+            --log --progress \
+            $@ \
+            task JER_Binning_Data \
+            --output-file-suffix "$OUTPUT_FILE_SUFFIX"
+    done
+
     # -- DATA (runs A, B, C with corr level L1L2Res)
 
     for _corr_level in "L1L2Res"; do
-        OUTPUT_FILE_SUFFIX="Z${_ch}_17Sep2018_Autumn18_JECV5_${_corr_level}"
+        OUTPUT_FILE_SUFFIX="ABC_Z${_ch}_${DATA_VERSION}_${JEC_VERSION}_${_corr_level}"
 
-        lumberjack.py -a zjet_excalibur -i "$INFILE_DATA" \
+        lumberjack.py -a zjet_excalibur -i "$INFILE_DATA_ABC" \
             --tree "basiccuts_${_corr_level}/ntuple" \
-            --type data \
+            --input-type data \
+            --selections "alpha" \
+            -j10 \
+            --log --progress \
+            $@ \
+            task JER_Binning_Data \
+            --output-file-suffix "$OUTPUT_FILE_SUFFIX"
+    done
+
+    # -- DATA (run D with corr level L1L2Res)
+
+    for _corr_level in "L1L2Res"; do
+        OUTPUT_FILE_SUFFIX="D_Z${_ch}_${DATA_VERSION}_${JEC_VERSION}_${_corr_level}"
+
+        lumberjack.py -a zjet_excalibur -i "$INFILE_DATA_D" \
+            --tree "basiccuts_${_corr_level}/ntuple" \
+            --input-type data \
             --selections "alpha" \
             -j10 \
             --log --progress \
